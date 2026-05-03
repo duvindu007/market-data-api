32eeae344b4a567b57555fa29b4d69333675ad37
duvindu007 <duvindupamaljithstu95@gmail.com>
2026-05-04 00:14:22 +0530

    add config.py for database and url for rest api

--- app/config.py
+++ app/config.py
@@ -1,2 +1,22 @@
-DB_NAME = "Market_data"
-EXTERNAL_API_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=IBM&apikey=demo"
+from pydantic_settings import BaseSettings,SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    # API
+    EXTERNAL_API_URL: str
+    API_KEY: str
+    API_TIMEOUT: int = 10
+
+    # Database
+    DB_NAME: str = "database/Market_data.db"
+
+    # Logging
+    LOG_LEVEL: str = "INFO"
+    LOG_FILE: str = "logs/app.log"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+# singleton instance
+settings = Settings()
+
