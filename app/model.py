18ab6859fe6a4747c05e7cd67fa0480d08c38f69
duvindu007 <duvindupamaljithstu95@gmail.com>
2026-05-04 02:59:27 +0530

    Updates to model class - remove unwanted classes

--- app/model.py
+++ app/model.py
@@ -2,23 +2,10 @@ from dataclasses import dataclass
 from pydantic import BaseModel, Field
 
 
-class MarketInput(BaseModel):
-    symbol_name: str
-    high: float
-    low: float
-    volume: float
-    month: str
-    year: str
-
-
 class MarketResponse(BaseModel):
     high: str = Field(coerce_numbers_to_str=True)
     low: str = Field(coerce_numbers_to_str=True)
     volume: str = Field(coerce_numbers_to_str=True)
-
-
-class SymbolInput(BaseModel):
-    symbol_name: str
 
 
 @dataclass
