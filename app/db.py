763f884c9f00e0f1aa9b1cf37af8fd7d0d8fbbc5
duvindu007 <duvindupamaljithstu95@gmail.com>
2026-05-04 00:16:07 +0530

    Add db.py where tables getting created

--- app/db.py
+++ app/db.py
@@ -1,39 +1,38 @@
 import sqlite3
-from app.config import DB_NAME
+from app.config import settings
+
 
 class DataBase:
-    def __init__(self, db_name = DB_NAME):
+    def __init__(self, db_name=settings.DB_NAME):
         self.db_name = db_name
 
     def get_connection(self):
-               conn = sqlite3.connect(self.db_name)
-               conn.execute("PRAGMA foreign_keys = ON")
-               return conn
-    
+        conn = sqlite3.connect(self.db_name)
+        conn.execute("PRAGMA foreign_keys = ON")
+        return conn
+
     def initiate_tables(self):
         conn = self.get_connection()
         cursor = conn.cursor()
 
         cursor.execute("""
-            CREATE TABLE IF NOT EXISTS symbols (
+            CREATE TABLE IF NOT EXISTS SYMBOLS (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 symbol_name TEXT
             )
         """)
 
-
         cursor.execute("""  
                 CREATE TABLE IF NOT EXISTS  TIME_SERIES_MONTHLY (
                        id INTEGER PRIMARY KEY AUTOINCREMENT,
                        symbol_id INTEGER,
-                       high REAL,
-                        low REAL,
-                        volume REAL,
+                       high TEXT,
+                        low TEXT,
+                        volume TEXT,
                         month TEXT,
                         year TEXT,
-                        FOREIGN KEY (symbol_id) REFERENCES job_types(id)
+                        FOREIGN KEY (symbol_id) REFERENCES SYMBOLS(id)
                        )
-        """
-                         )
+        """)
         conn.commit()
         conn.close()
